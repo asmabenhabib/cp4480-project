912affa2c40e32d49b80f12a90f3cd03137e8ac7
Addalin <77745051+Addalin-CP3445@users.noreply.github.com>
2021-04-01 20:47:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var mysql = require('mysql');
 let connection = async (params) => new Promise(
     (resolve, reject) => {
         const connection = mysql.createConnection({
-            host: 'localhost',
+            host: 'http://192.168.0.110',
             user: 'root',
             password: 'password',
             database: 'Messagesproject',
