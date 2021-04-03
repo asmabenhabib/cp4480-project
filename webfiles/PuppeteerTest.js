94a626ddd715f39f12be7201c363e6fbc480e15c
Addalin <60094638@cna-qatar.edu.qa>
2021-04-03 13:41:39 +0300

    updated puppeteer

--- webfiles/PuppeteerTest.js
+++ webfiles/PuppeteerTest.js
@@ -3,82 +3,100 @@ async function testlogin(user, pass) {
     const browser = await puppeteer.launch({
         headless: true,
         args: ['--no-sandbox']
-     })
+    })
     const page = await browser.newPage()
-    await page.goto("http://192.168.100.5:8000")
-    await page.focus('#username')
-    await page.keyboard.type(user)
-    await page.focus('#password')
-    await page.keyboard.type(pass)
-    let loginButton = await page.$('#login-button')
-    await loginButton.click()
-    await page.waitForNavigation()
-    let result = await page.url()
-    testChat(result, page)
+    try {
+        await page.goto("http://192.168.100.5:8000")
+        await page.focus('#username')
+        await page.keyboard.type(user)
+        await page.focus('#password')
+        await page.keyboard.type(pass)
+        let loginButton = await page.$('#login-button')
+        await loginButton.click()
+        await page.waitForNavigation()
+        let result = await page.url()
+        testChat(result, page)
+    }
+    catch (e) {
+        console.log(e)
+        process.exit(1)
+    }
 
 }
 
 async function testChat(result, page) {
-    if(result=='http://192.168.100.5:8000/admin.html') {
+    if (result == 'http://192.168.100.5:8000/admin.html') {
         await page.goto(result, {
-    
+
             waitUntil: ['networkidle2', 'load']
         });
-        let user = await page.$('#user1')
-        await user.click()
-        await page.waitForSelector('#currenMessage')
-        await page.focus('#currenMessage')
-        await page.keyboard.type('new Message')
-        let sendMessage = await page.$('#sendMessage')
-        await sendMessage.click()
-        let Allchats = await page.$('#chats')
-        await Allchats.click()
-        await page.waitForSelector('#chat1')
-        let chat = await page.$('#chat1')
-        await chat.click()
-        await page.waitForSelector('#currenMessage')
-        await page.focus('#currenMessage')
-        await page.keyboard.type('helloWorld:)')
-        let sendnewMessage = await page.$('#sendMessag')
-        await sendnewMessage.click()
-        let AdminChat= await page.$('#chats1')
-        await AdminChat.click()
-        await page.waitForSelector('#userChat1')
-        let Userschat = await page.$('#userChat1')
-        await Userschat.click()
-        let logout= await page.$('#logout-button')
-        await logout.click()
-        testlogin('user1', 'password1')
-        
-    } 
+        try {
+            let user = await page.$('#user1')
+            await user.click()
+            await page.waitForSelector('#currenMessage')
+            await page.focus('#currenMessage')
+            await page.keyboard.type('new Message')
+            let sendMessage = await page.$('#sendMessage')
+            await sendMessage.click()
+            let Allchats = await page.$('#chats')
+            await Allchats.click()
+            await page.waitForSelector('#chat1')
+            let chat = await page.$('#chat1')
+            await chat.click()
+            await page.waitForSelector('#currenMessage')
+            await page.focus('#currenMessage')
+            await page.keyboard.type('helloWorld:)')
+            let sendnewMessage = await page.$('#sendMessag')
+            await sendnewMessage.click()
+            let AdminChat = await page.$('#chats1')
+            await AdminChat.click()
+            await page.waitForSelector('#userChat1')
+            let Userschat = await page.$('#userChat1')
+            await Userschat.click()
+            let logout = await page.$('#logout-button')
+            await logout.click()
+            testlogin('user1', 'password1')
+        }
+        catch (e) {
+            console.log(e)
+            process.exit(1)
+        }
 
-    if(result=='http://192.168.100.5:8000/mainpage.html') {
+    }
+
+    if (result == 'http://192.168.100.5:8000/mainpage.html') {
         await page.goto(result, {
-    
+
             waitUntil: ['networkidle2', 'load']
         });
-        let user = await page.$('#user2')
-        await user.click()
-        await page.waitForSelector('#currenMessage')
-        await page.focus('#currenMessage')
-        await page.keyboard.type('new Message')
-        let sendMessage = await page.$('#sendMessage')
-        await sendMessage.click()
-        let Allchats = await page.$('#chats')
-        await Allchats.click()
-        await page.waitForSelector('#chat2')
-        let chat = await page.$('#chat2')
-        await chat.click()
-        await page.waitForSelector('#currenMessage')
-        await page.focus('#currenMessage')
-        await page.keyboard.type('helloWorld:)')
-        let sendnewMessage = await page.$('#sendMessage')
-        await sendnewMessage.click()
+        try {
+            let user = await page.$('#user2')
+            await user.click()
+            await page.waitForSelector('#currenMessage')
+            await page.focus('#currenMessage')
+            await page.keyboard.type('new Message')
+            let sendMessage = await page.$('#sendMessage')
+            await sendMessage.click()
+            let Allchats = await page.$('#chats')
+            await Allchats.click()
+            await page.waitForSelector('#chat2')
+            let chat = await page.$('#chat2')
+            await chat.click()
+            await page.waitForSelector('#currenMessage')
+            await page.focus('#currenMessage')
+            await page.keyboard.type('helloWorld:)')
+            let sendnewMessage = await page.$('#sendMessage')
+            await sendnewMessage.click()
 
-        let logout= await page.$('#logout-button')
-        await logout.click()
-        process.exit(0)
-    } 
+            let logout = await page.$('#logout-button')
+            await logout.click()
+            process.exit(0)
+        }
+        catch (e) {
+            console.log(e)
+            process.exit(1)
+        }
+    }
 }
 
-testlogin('admin','passwordadmin')
+testlogin('admin', 'passwordadmin')
